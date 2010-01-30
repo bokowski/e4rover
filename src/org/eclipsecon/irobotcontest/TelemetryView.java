7d2517e55f0c7db0ea5f1facf088bce878895ba1
Boris Bokowski <bokowski@gmail.com>
2010-01-30 16:59:56 -0500

    Initial commit

---
+++ src/org/eclipsecon/irobotcontest/TelemetryView.java
@@ -0,0 +1,54 @@
+package org.eclipsecon.irobotcontest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.inject.Inject;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+
+public class TelemetryView {
+
+	@Inject
+	public TelemetryView(final Composite parent) throws IOException {
+		parent.setLayout(new FillLayout());
+		final Text text = new Text(parent, SWT.MULTI | SWT.V_SCROLL | SWT.BORDER);
+		final URL url = new URL(
+				"http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=CYOW");
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						InputStream stream = url.openStream();
+						byte[] buffer = new byte[4096];
+						int len;
+						final StringBuffer content = new StringBuffer();
+						while ((len = stream.read(buffer, 0, buffer.length)) > 0) {
+							content.append(new String(buffer, 0, len));
+						}
+						parent.getDisplay().asyncExec(new Runnable() {
+							@Override
+							public void run() {
+								text.setText(content.toString());
+							}
+						});
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					try {
+						Thread.sleep(100000);
+					} catch (InterruptedException e) {
+						return;
+					}
+				}
+			}
+		};
+		t.setDaemon(true);
+		t.start();
+	}
+}
---
+++ src/org/eclipsecon/irobotcontest/WebcamView.java
@@ -0,0 +1,64 @@
+package org.eclipsecon.irobotcontest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.inject.Inject;
+
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+
+public class WebcamView {
+
+	@Inject
+	public WebcamView(final Composite parent) throws IOException {
+		final Image[] image = new Image[1];
+		parent.setLayout(new FillLayout());
+		parent.addPaintListener(new PaintListener() {
+
+			@Override
+			public void paintControl(PaintEvent e) {
+				if (image[0] != null) {
+					e.gc.drawImage(image[0], 0, 0);
+				}
+			}
+		});
+		final URL url = new URL(
+				"http://www.collineduparlement-parliamenthill.gc.ca/text/newhillcam.jpg");
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					InputStream stream;
+					try {
+						stream = url.openStream();
+						ImageLoader il = new ImageLoader();
+						ImageData[] imageData = il.load(stream);
+						image[0] = new Image(parent.getDisplay(), imageData[0]);
+						parent.getDisplay().asyncExec(new Runnable() {
+							@Override
+							public void run() {
+								parent.redraw();
+							}
+						});
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						return;
+					}
+				}
+			}
+		};
+		t.setDaemon(true);
+		t.start();
+	}
+}
