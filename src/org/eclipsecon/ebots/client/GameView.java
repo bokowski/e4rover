d1fb97ff4763b2a067a5485e3a8063f869218fa1
Brian de Alwis <bsd@acm.org>
2010-02-12 21:03:03 -0500

    Hooked the GameController into the e4 client so that the client is now running "live".

--- src/org/eclipsecon/ebots/client/GameView.java
+++ src/org/eclipsecon/ebots/client/GameView.java
@@ -1,54 +1,48 @@
 package org.eclipsecon.ebots.client;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import javax.inject.Inject; 
 
-import javax.inject.Inject;
-
+import org.eclipse.e4.core.services.annotations.PostConstruct;
+import org.eclipse.e4.core.services.annotations.PreDestroy;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import org.eclipsecon.ebots.core.ContestPlatform;
+import org.eclipsecon.ebots.core.IGame;
+import org.eclipsecon.ebots.core.IUpdateListener;
+import org.eclipsecon.ebots.core.UpdateAdapter;
 
 public class GameView {
-
-	@Inject
-	public GameView(final Composite parent) throws IOException {
+	@Inject Composite parent;
+	protected IUpdateListener listener;
+	
+	@PostConstruct
+	public void init() {
 		parent.setLayout(new FillLayout());
 		final Text text = new Text(parent, SWT.MULTI | SWT.V_SCROLL | SWT.BORDER);
-		final URL url = new URL(
-				"http://ebots.s3.amazonaws.com/game.xml");
-		Thread t = new Thread() {
+
+		ContestPlatform cp = ContestPlatform.getDefault();
+		cp.addUpdateListener(listener = new UpdateAdapter() {
 			@Override
-			public void run() {
-				while (true) {
-					try {
-						InputStream stream = url.openStream();
-						byte[] buffer = new byte[4096];
-						int len;
-						final StringBuffer content = new StringBuffer();
-						while ((len = stream.read(buffer, 0, buffer.length)) > 0) {
-							content.append(new String(buffer, 0, len));
+			public void gameUpdated(final IGame game) {
+				parent.getDisplay().asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						if(!text.isDisposed()) {
+							text.setText(game.toString());
 						}
-						parent.getDisplay().asyncExec(new Runnable() {
-							@Override
-							public void run() {
-								text.setText(content.toString());
-							}
-						});
-					} catch (IOException e) {
-						e.printStackTrace();
 					}
-					try {
-						Thread.sleep(100000);
-					} catch (InterruptedException e) {
-						return;
-					}
-				}
-			}
-		};
-		t.setDaemon(true);
-		t.start();
+				});
+			}});		
+	}
+	
+	@PreDestroy
+	public void dispose() {
+		if(listener != null) {
+			ContestPlatform cp = ContestPlatform.getDefault();
+			cp.removeUpdateListener(listener);
+		}
+		listener = null;
 	}
 }
