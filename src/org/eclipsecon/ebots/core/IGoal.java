a44c22bae9ff7e3dd9a36e8c32530a98bc2b4f36
jnorris <jnorris@192.168.0.101>
2010-02-15 15:47:35 -0800

    Rebranded Shots, Goals, and Pucks as Goals, Targets, and Instruments.

---
+++ src/org/eclipsecon/ebots/core/IGoal.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipsecon.ebots.core;
+
+/**
+ * A goal represents a task to be performed by the robot. In particular, a goal
+ * involves aligning an instrument held by the robot with a target attached to
+ * the perimeter of the site.
+ */
+public interface IGoal {
+	/**
+	 * The TARGET enumeration lists the target RFID readers available on the site
+	 * perimeter. The goals are at 0, 120, and 240 degrees around the arena perimeter
+	 */
+	public enum TARGET {G1, G2,G3}
+
+	/**
+	 * The INSTRUMENTs are represented as RFID tags around the perimeter of the
+	 * robot at 45, 135, 225, and 315 degree. Points are scored by aligning an
+	 * instrument on the robot with a target on the site perimeter.
+	 */
+	public enum INSTRUMENT {A, B,C, D}
+
+	/**
+	 * Returns the target for this goal.
+	 * @return the target
+	 */
+	public TARGET getTarget();
+
+	/**
+	 * Returns the instrument for this goal
+	 * @return the instrument.
+	 */
+	public INSTRUMENT getInstrument();
+}
--- src/org/eclipsecon/ebots/internal/core/Game.java
+++ src/org/eclipsecon/ebots/internal/core/Game.java
@@ -1,7 +1,7 @@
 package org.eclipsecon.ebots.internal.core;
 
 import org.eclipsecon.ebots.core.IGame;
-import org.eclipsecon.ebots.core.IShot;
+import org.eclipsecon.ebots.core.IGoal;
 
 public class Game extends ServerObject implements IGame{
 
@@ -12,8 +12,8 @@ public class Game extends ServerObject implements IGame{
 	int score;
 	int countdownSeconds;
 	int remainingSeconds;
-	Shot lastShot = null;
-	Shot nextShot = null;
+	Goal lastGoal = null;
+	Goal nextGoal = null;
 	int nextReward;
 	private long gameStartTimeMillis;
 	private long gameEndTimeMillis;
@@ -34,12 +34,12 @@ public class Game extends ServerObject implements IGame{
 		return remainingSeconds;
 	}
 
-	public IShot getLastShot() {
-		return lastShot;
+	public IGoal getLastGoal() {
+		return lastGoal;
 	}
 
-	public IShot getNextShot() {
-		return nextShot;
+	public IGoal getNextGoal() {
+		return nextGoal;
 	}
 
 	public int getNextReward() {
@@ -51,15 +51,26 @@ public class Game extends ServerObject implements IGame{
 		return null;
 	}
 
-	/* Game state changers - called only by server */
+	/* Game state changers - called only by server. */
 
+	/**
+	 * Called when a player has been identified and it is time to count down to
+	 * the start of the game.
+	 */
 	public void enterCountdownState(String playerName) {
 		System.out.println("Entering countdown state...");
 		this.playerName = playerName;
 		countdownSeconds = IGame.COUNTDOWN_SECONDS;
 		gameStartTimeMillis = System.currentTimeMillis() + countdownSeconds*1000;
 	}
-	
+
+	/**
+	 * Advances the clock that is counting down the seconds before the beginning
+	 * of the game. Returns true if more time remains, false otherwise.
+	 * 
+	 * @return true if more seconds remain before the game will begin, false if
+	 *         the game has started or if no game is in progress.
+	 */
 	public boolean tickCountdownClock() {
 		countdownSeconds = Math.round((gameStartTimeMillis - System.currentTimeMillis())/1000);
 		if (countdownSeconds < 0) {
@@ -68,18 +79,29 @@ public class Game extends ServerObject implements IGame{
 		}
 		return true;
 	}
-	
+
+	/**
+	 * Called when the game begins, after the pre-game countdown has finished.
+	 * Only called by the server.
+	 */
 	public void enterPlayingState() {
 		System.out.println("Entering playing state...");
 		countdownSeconds = 0;
 		remainingSeconds = GAME_LENGTH_SECONDS;
-		lastShot = null;
-		nextShot = new Shot();
+		lastGoal = null;
+		nextGoal = new Goal();
 		nextReward = FIRST_REWARD;
 		score = 0;
 		gameEndTimeMillis = System.currentTimeMillis() + (remainingSeconds * 1000);
 	}
 
+	/**
+	 * Advances the clock that is counting down the seconds to the conclusion of
+	 * the game. Returns true if more time remains, false otherwise.
+	 * 
+	 * @return true if more seconds remain on the clock, false if time has
+	 *         elapsed or no game is in progress
+	 */
 	public boolean tickPlayClock() {
 		remainingSeconds = Math.round((gameEndTimeMillis - System.currentTimeMillis())/1000);
 		if (remainingSeconds < 0) {
@@ -88,27 +110,40 @@ public class Game extends ServerObject implements IGame{
 		}
 		return true;
 	}
-	
-	public void handleShot(IShot shot) {
-		System.out.println("Handling " + shot);
-		if (shot.equals(nextShot)) {
-			System.out.println("Shot is good!");
+
+	/**
+	 * Called when a goal attempt has been detected by the server. It is compared
+	 * against the expected goal and if it matches, the player's score is
+	 * increased appropriately.
+	 * 
+	 * @param goal the goal that has been detected
+	 */
+	public void handleGoal(IGoal goal) {
+		System.out.println("Handling " + goal);
+		if (goal.equals(nextGoal)) {
+			System.out.println("Goal is good!");
 			score += nextReward;
 			nextReward++;
 		} else {
-			System.out.println("Shot is bad. Expected " + nextShot);
+			System.out.println("Goal is bad. Expected " + nextGoal);
 			nextReward = Game.FIRST_REWARD;
 		}
-		// time for next shot
-		lastShot = nextShot;
-		nextShot = new Shot();
+		// time for next goal
+		lastGoal = nextGoal;
+		nextGoal = new Goal();
 	}
-	
+
+	/**
+	 * Called after the game ends. Resets the game state in preparation for the
+	 * next player.
+	 */
 	public void enterGameOverState() {
 		remainingSeconds = 0;
-		lastShot = null;
-		nextShot = null;
-		nextReward = 0;
+		lastGoal = null;
+		nextGoal = null;
+		nextReward = -1;
+		score = -1;
+		playerName = null;
 	}
 	
 }
