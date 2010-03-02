7895d057e0a7dae88be5d50494fd5d5d457988f6
Brian de Alwis <bsd@acm.org>
2010-03-02 13:00:13 -0500

    Added two new views to report the player queue and the players list. In XML, of course!

---
+++ org.eclipsecon.ebots.client/src/org/eclipsecon/ebots/client/PlayersQueueView.java
@@ -0,0 +1,51 @@
+package org.eclipsecon.ebots.client;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.annotations.PostConstruct;
+import org.eclipse.e4.core.services.annotations.PreDestroy;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipsecon.ebots.core.ContestPlatform;
+import org.eclipsecon.ebots.core.IGame;
+import org.eclipsecon.ebots.core.IPlayerQueue;
+import org.eclipsecon.ebots.core.IUpdateListener;
+import org.eclipsecon.ebots.core.UpdateAdapter;
+
+public class PlayersQueueView extends Object {
+	@Inject Composite parent;
+	protected IUpdateListener listener;
+	
+	@PostConstruct
+	public void init() {
+		parent.setLayout(new FillLayout());
+		final Text text = new Text(parent, SWT.MULTI | SWT.V_SCROLL | SWT.BORDER);
+
+		ContestPlatform cp = ContestPlatform.getDefault();
+		cp.addUpdateListener(listener = new UpdateAdapter() {
+			@Override
+			public void playerQueueUpdated(final IPlayerQueue queue) {
+				if(parent.isDisposed()) { return; }
+				parent.getDisplay().asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						if(!parent.isDisposed()) {
+							text.setText(queue.toString());
+						}
+					}
+				});
+			}});		
+	}
+	
+	@PreDestroy
+	public void dispose() {
+		if(listener != null) {
+			ContestPlatform cp = ContestPlatform.getDefault();
+			cp.removeUpdateListener(listener);
+		}
+		listener = null;
+	}
+
+}
