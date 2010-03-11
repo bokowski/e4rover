878966a4f8902e1bece43c4cda3d2ff8dda61a58
Boris Bokowski <bokowski@gmail.com>
2010-03-11 02:55:38 -0500

    copyright bash

--- org.eclipsecon.e4rover.common/src/org/eclipsecon/e4rover/IServer.java
+++ org.eclipsecon.e4rover.common/src/org/eclipsecon/e4rover/IServer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Brian de Alwis and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Brian de Alwis - initial API and implementation
+ ******************************************************************************/
 package org.eclipsecon.e4rover;
 
 import java.io.IOException;
--- org.eclipsecon.e4rover.common/src/org/eclipsecon/e4rover/core/IServerConstants.java
+++ org.eclipsecon.e4rover.common/src/org/eclipsecon/e4rover/core/IServerConstants.java
@@ -1,5 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
 package org.eclipsecon.e4rover.core;
-
 
 /**
  * Constants for accessing key resources on the server.  Users should not need to edit this file.
