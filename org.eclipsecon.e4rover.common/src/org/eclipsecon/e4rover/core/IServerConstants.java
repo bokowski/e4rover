f2d5b28ddc532ab7bc0534a083f55606fb8afc64
Boris Bokowski <bokowski@gmail.com>
2010-03-12 16:29:31 -0500

    changed URL ebots->e4rover

--- org.eclipsecon.e4rover.common/src/org/eclipsecon/e4rover/core/IServerConstants.java
+++ org.eclipsecon.e4rover.common/src/org/eclipsecon/e4rover/core/IServerConstants.java
@@ -33,7 +33,7 @@ public interface IServerConstants {
 	public static final String IMAGE_FILE_URI = E4ROVER_DOWNLINK_SERVER_URI + IMAGE_FILE_NAME;
 	
 	/* Uplink */
-	public static final String EROVER_UPLINK_SERVER_URI ="http://ebots.eclipsecon.org/";
+	public static final String EROVER_UPLINK_SERVER_URI ="http://e4rover.eclipsecon.org/";
 //	public static final String EROVER_UPLINK_SERVER_URI ="http://137.78.29.221:9111/";
 	
 	public static final String COMMAND_RESTLET_URI = EROVER_UPLINK_SERVER_URI + "cmd/";
