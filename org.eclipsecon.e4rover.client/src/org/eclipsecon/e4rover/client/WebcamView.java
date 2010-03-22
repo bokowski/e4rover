4f36a43620fbc41256afd1c57fa298641ab1d1ae
Boris Bokowski <bokowski@gmail.com>
2010-03-21 22:46:16 -0400

    fixed fliker

--- org.eclipsecon.e4rover.client/src/org/eclipsecon/e4rover/client/WebcamView.java
+++ org.eclipsecon.e4rover.client/src/org/eclipsecon/e4rover/client/WebcamView.java
@@ -17,13 +17,14 @@ import javax.inject.Inject;
 
 import org.eclipse.e4.core.services.annotations.EventHandler;
 import org.eclipse.e4.core.services.annotations.PostConstruct;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipsecon.e4rover.core.IArenaCamImage;
 
 public class WebcamView {
@@ -33,6 +34,8 @@ public class WebcamView {
 
 	protected Image image;
 
+	protected Control canvas;
+
 	/*
 	 * Methods annotated with @PostConstruct will be called after all values
 	 * have been injected successfully. The equivalent for in 3.x would be
@@ -41,7 +44,8 @@ public class WebcamView {
 	@PostConstruct public void init() throws IOException {
 
 		parent.setLayout(new FillLayout());
-		parent.addPaintListener(new PaintListener() {
+		canvas = new Canvas(parent, SWT.NO_BACKGROUND);
+		canvas.addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent e) {
 				if (image != null) {
 					e.gc.drawImage(image, 0, 0);
@@ -67,16 +71,16 @@ public class WebcamView {
 		if (parent.isDisposed()) {
 			return;
 		}
-		ImageLoader il = new ImageLoader();
-		ImageData[] imageData = il.load(new ByteArrayInputStream(img.getImage()));
+		Image newImage = new Image(parent.getDisplay(), new ByteArrayInputStream(img.getImage()));
 		if (image != null && !image.isDisposed()) {
 			Image toBeDisposed = image;
-			image = null;
+			image = newImage;
 			toBeDisposed.dispose();
+		} else {
+			image = newImage;
 		}
-		image = new Image(parent.getDisplay(), imageData[0]);
 		redraw();
-	};
+	}
 
 	void redraw() {
 		if (parent.isDisposed()) {
@@ -87,7 +91,7 @@ public class WebcamView {
 				if (parent.isDisposed()) {
 					return;
 				}
-				parent.redraw();
+				canvas.redraw();
 			}
 		});
 	}
